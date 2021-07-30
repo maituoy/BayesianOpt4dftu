080cd62ff088b52e637176a5e377af238740d3b4
Maituo Yu <37908868+maituoy@users.noreply.github.com>
2021-07-30 14:24:11 +0100

    minor changes

--- example/2d/example.py
+++ example/2d/example.py
@@ -53,8 +53,9 @@ def main():
 	
 	if os.path.exists('./u_tmp.txt'):
 		os.remove('./u_tmp.txt')
-		with open('./u_tmp.txt', 'w') as f:
-			f.write('%s band_gap(eV) delta_band(eV) \n' % (' '.join(header)))
+		
+	with open('./u_tmp.txt', 'w+') as f:
+		f.write('%s band_gap(eV) delta_band(eV) \n' % (' '.join(header)))
 
 	obj = 0 
 	threshold = args.threshold
